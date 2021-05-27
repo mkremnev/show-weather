4e1c8a6f7cfb828f0d6cc80ce66040bfe7128c1e
Maxim Kremnev <m.kremnev@netlinux.ru>
2021-05-27 12:45:42 +0300

    Updating CityInformation component

--- src/js/components/CityInformation.js
+++ src/js/components/CityInformation.js
@@ -1,4 +1,19 @@
-export function CityInformation(el) {
+export function CityInformation(el, city) {
+  const month = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
   el.insertAdjacentHTML(
     'beforeend',
     `
@@ -12,11 +27,16 @@ export function CityInformation(el) {
           "
         >
           <div class="city-information-precipValue">
-            <div>Mostly sunny</div>
-            <div>Friday, 30 April</div>
+            <img src="http://openweathermap.org/img/wn/${
+              city?.weather[0].icon
+            }@2x.png" alt="${city?.weather.description}">
+            <div>${city?.weather[0].description}</div>
+            <div>${month[new Date(city?.dt * 1000).getMonth()]}, ${new Date(
+      city?.dt * 1000
+    ).getDate()}</div>
           </div>
           <div class="city-information-tempValue">
-            <div class="temp-value">40</div>
+            <div class="temp-value">${Math.floor(city?.main.temp)}</div>
             <div class="temp-value-degree"><sup>0</sup>C</div>
           </div>
         </section>
